e13adde4907a1bc48a6f3546f5e8fc57f3e5ec5b
Bryce Huey <>
2021-09-24 08:54:22 -0500

    the herokuapp download links changed, updated the link

--- test/download_test.exs
+++ test/download_test.exs
@@ -2,7 +2,7 @@ defmodule DownloadTest do
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case
-  
+
   Code.require_file("reuseableCode/reuseables.exs")
 
   # Start hound session and destroy when tests are run
@@ -20,13 +20,13 @@ defmodule DownloadTest do
   test "download and verify file exists" do
 
     navigate_to "https://the-internet.herokuapp.com/download"
-    file = find_element(:xpath, "//*[text() ='loremipsum.txt']")
+    file = find_element(:xpath, "//*[text() ='SampleUpload.txt']")
     click(file)
     #time to download
     :timer.sleep(3000)
 
     #verifies existence of downloaded file and clears directory so ready for next test
-    File.read!("downloads/loremipsum.txt")
+    File.read!("downloads/SampleUpload.txt")
     File.rm_rf("downloads")
 
   end
